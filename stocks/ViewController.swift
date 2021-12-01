37ba76fb9978dc684f6bf7349849163e2d6ecbb1
Admin <admin@admins-macbook-pro.local>
2021-12-01 10:56:19 +0200

    Initial Commit

---
+++ stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stocks
+//
+//  Created by Admin on 1.12.21.
+//  Copyright © 2021 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
